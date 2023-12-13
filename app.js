184a8689d5a620c82e837120bd7e71ade7c68b05
DanDreger <trigger125@hotmail.com>
2023-12-13 14:01:01 -0700

    remove config from js files

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDD_dZ3mGLRx5iIZkeTdig8WcPA6M_5WFw",
-    authDomain: "player-tracker-816e1.firebaseapp.com",
-    projectId: "player-tracker-816e1",
-    storageBucket: "player-tracker-816e1.appspot.com",
-    messagingSenderId: "252085866739",
-    appId: "1:252085866739:web:c2e73a62787bb7a636a786"
-};
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
 // Function to handle form submission
